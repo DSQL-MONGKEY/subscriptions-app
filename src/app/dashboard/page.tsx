902b2282bfeca3ab3fd8d1f0103994e0a7b3b8e0
Dimpfe <dimasprasetyo7696@gmail.com>
2024-08-03 21:39:30 +0700

    chore<>: add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/Hero";
+import { Pricing } from "@/components/Pricing";
+
+export default function Home() {
+	return (
+		<main>
+			<h1 className="text-center text-2xl">Ini Dashboard</h1>
+			<Hero />
+		</main>
+	);
+}
